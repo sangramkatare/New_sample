45d2c41e8710d25b8043f5425737d0e2de1249f2
Sangramsinh Katare <sangramkatare2003@gmail.com>
2024-07-18 13:01:27 +0530

    interface assignment

---
+++ New_sample/Interface_Ass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_sample
+{
+    public interface Iplayable
+    {
+        void Play();
+    }
+    public class VedioPlayer : Iplayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Vedio....\n");
+        }
+    }
+    public class MusicPlayer : Iplayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing music....");
+
+        }
+    }
+
+    internal interface Interface_Ass
+    {
+        static void Main()
+        {
+            VedioPlayer v = new VedioPlayer();
+            MusicPlayer m = new MusicPlayer();
+
+            v.Play();
+            m.Play();
+        }
+    }
+}
