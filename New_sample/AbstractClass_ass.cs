53f290e7738a99163c62b3d028a4f69e27da7d64
Sangramsinh Katare <sangramkatare2003@gmail.com>
2024-07-18 08:40:24 +0530

    Interface program

---
+++ New_sample/AbstractClass_ass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_sample
+{
+    public abstract class Shapes
+    {
+        public abstract double CalculateArea();
+
+        public void Display()
+        {
+            Console.WriteLine("This is a Details of " + GetType().Name);
+        }
+    }
+    public class Rectangles : Shapes
+    {
+        public override double CalculateArea()
+        {
+            double length = 11.2;
+            double breadth = 3.4;
+            return (length * breadth);
+        } 
+    }
+    public class Circles : Shapes
+    {
+        public override double CalculateArea()
+        {
+            double radius = 2.2;
+            return (3.14 *( radius * radius));
+        }
+    }
+
+    internal class AbstractClass_ass
+    {
+        static void Main (string[] args)
+        {
+            // define objects
+            Rectangles rec = new Rectangles();
+            Circles cir = new Circles();
+            
+            // rectangle Area
+            rec.Display();
+            double R = rec.CalculateArea();
+            Console.WriteLine("Area of Rectangle: "+ R);
+
+            Console.WriteLine(); // Blank line for separation
+
+            // circle area
+            cir.Display();
+            double C = cir.CalculateArea();
+            Console.WriteLine("Area of Circle: " + C);
+        }
+    }
+}
---
+++ New_sample/TestInterface.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_sample
+{
+    public interface Logger
+    {
+        void LogMessage(string message);
+
+    }
+    public interface TestLogger
+    {
+        void TestLogMessage(string message);
+
+    }
+    public class Consolelogger : Logger,TestLogger   // over come the problem of multiple inheritance
+    {
+        public void LogMessage(string messsage)
+        {
+            Console.WriteLine($"Logging as message:{messsage}");
+        }
+        public void TestLogMessage(string address)
+        {
+            Console.WriteLine($"Logging as message:{address}");
+        }
+    }
+    internal interface TestInterface
+    {
+        static void Main(string[] args)
+        {
+            Consolelogger cl=new Consolelogger();
+            cl.LogMessage("hello World");
+            cl.TestLogMessage("I am in pune");
+
+        }
+     
+    }
+}
