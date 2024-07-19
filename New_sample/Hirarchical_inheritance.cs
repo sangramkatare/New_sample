d1a3436edcb37ceb683b66e4c6dbb53e6b8939e5
Sangramsinh Katare <sangramkatare2003@gmail.com>
2024-07-19 08:36:10 +0530

    Inheritance

---
+++ New_sample/Hirarchical_inheritance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_sample
+{
+    public class Animal1
+    {
+        public void Eat1()
+        {
+            Console.WriteLine("eating");
+        }
+    }
+    public class Dog1: Animal1
+    {
+        public void Barking1()
+        {
+            Console.WriteLine("Barking");
+        }
+    }
+    public class Cat1 : Animal1
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Meow meow");
+        }
+    }
+
+    internal class Hirarchical_inheritance
+    {
+        static void Main()
+        {
+            
+            Dog1 a = new Dog1();
+            a.Eat1();
+            a.Barking1();
+
+            Cat1 c = new Cat1();
+            c.Eat1();
+            c.Meow();
+        }
+    }
+}
---
+++ New_sample/Multilevelinheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_sample
+{
+    public class A
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+    }
+    public class B : A
+    {
+        public void Drink()
+        {
+            Console.WriteLine("Drinking");
+        }
+    }
+        public class C:B
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Barking");
+            }
+        }
+
+    internal class Multilevelinheritance
+    {
+        static void Main()
+        {
+            C a=new C();
+            a.Eat();
+            a.Drink();
+            a.Bark();
+        }
+
+    }
+}
